3e2d0b932278ede18d33edde962b5da0cbc75a44
Helal Hossain <71216025+helal88hossain@users.noreply.github.com>
2021-01-09 13:08:59 +0600

    edit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+        $('.ProgressHtml').rProgressbar({
+        percentage: 90,
+        fillBackgroundColor: '#754ef9',
+        backgroundColor: '#cccccc',
+        borderRadius: '5px',
+		height: '5px',
+        duration: 2000
+        });
+
+        $('.ProgressCss').rProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#754ef9',
+        backgroundColor: '#cccccc',
+        borderRadius: '5px',
+		height: '5px',
+        duration: 2000
+        });
+
+        $('.ProgressBootstrap').rProgressbar({
+        percentage: 75,
+        fillBackgroundColor: '#754ef9',
+        backgroundColor: '#cccccc',
+        borderRadius: '5px',
+		height: '5px',
+        duration: 2000
+        });
+
+        $('.ProgressJs').rProgressbar({
+        percentage: 60,
+        fillBackgroundColor: '#754ef9',
+        backgroundColor: '#cccccc',
+        borderRadius: '5px',
+		height: '5px',
+        duration: 2000
+        });
+
+});
